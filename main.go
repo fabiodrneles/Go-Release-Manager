eb11ecbc8ed33e651a86f6eef13cde09124325fd
fabiodrneles <fabiodrneles@gmail.com>
2025-10-24 16:49:17 -0300

    fix: testando consulta de versão

--- main.go
+++ main.go
@@ -4,23 +4,19 @@ import (
 	"go-release-manager/cmd"
 )
 
-// --- NOVO ---
-// Estas variáveis serão preenchidas pelo GoReleaser durante o build
-// usando a flag -ldflags (ex: -X main.version=v1.2.3).
-// Veja a configuração em .goreleaser.yml
 var (
-	version = "dev"  // Valor padrão se não for buildado com GoReleaser
-	commit  = "none" // Valor padrão
-	// date    = "unknown" // Você pode adicionar a data também se quiser
+	version string
+	commit  string
 )
 
-// --- FIM DO NOVO ---
-
 func main() {
-	// --- NOVO ---
-	// Passa a versão para o pacote cmd para que ele possa exibi-la
-	cmd.SetVersionInfo(version, commit)
-	// --- FIM DO NOVO ---
+	if version == "" {
+		version = "dev"
+	}
+	if commit == "" {
+		commit = "none"
+	}
 
+	cmd.SetVersionInfo(version, commit)
 	cmd.Execute()
 }
